53cdc630540b42edd947802c221786dd0bf2c1d3
sergiocardoso1 <sergioifbaiano@hotmail.com>
2023-02-23 12:41:14 -0300

    test

---
+++ js/script.js
@@ -0,0 +1,41 @@
+ //menu
+ const btnMobile = document.getElementById('btn-mobile');
+ const btnMenu = document.getElementById('btn-menu')
+ const btnMenu2 = document.getElementById('btn-menu2')
+ const btnMenu3 = document.getElementById('btn-menu3')
+ const btnMenu4 = document.getElementById('btn-menu4')
+
+ function toggleMenu(){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+ }
+
+ btnMobile.addEventListener('click', toggleMenu);
+
+ btnMenu.addEventListener('click',function(){
+   nav.classList.toggle('active');
+ });
+
+ btnMenu2.addEventListener('click',function(){
+   nav.classList.toggle('active');
+ });
+
+ btnMenu3.addEventListener('click',function(){
+   nav.classList.toggle('active');
+ });
+
+ btnMenu4.addEventListener('click',function(){
+   nav.classList.toggle('active');
+ });
+
+
+//slider
+ let contador = 1;
+
+ setInterval(function(){
+   document.getElementById('slide' + contador).checked = true;
+   contador++;
+   if(contador > 4){
+      contador = 1;
+   }
+ },4000)
